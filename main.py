677b13815053ac3704ccc9eff9972804774d66a5
suchith Krishna S Donni <sksdonni18@gmail.com>
2021-04-02 23:30:55 +0530

    project changes

--- main.py
+++ main.py
@@ -10,21 +10,6 @@ def index():
 def about():
 	return render_template('about.html')
 
-@app.route('/blog')
-def blog():
-	return render_template('blog.html')
-
-@app.route('/work')
-def work():
-	return render_template('work.html')
-
-@app.route('/my_life')
-def my_hobbies():
-	return render_template('life.html')
-
-@app.route('/admin')
-def admin():
-	return render_template('admin.html')
 
 if __name__ == '__main__':
 	app.run(debug=1)
